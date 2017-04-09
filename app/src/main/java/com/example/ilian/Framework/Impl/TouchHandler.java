70b43548495a7376721c13d6933a2a5dfb898f79
ilian <heatblazer@gmail.com>
2017-04-09 21:14:05 +0300

    TouchHandler interface extends android touch event interface

--- app/src/main/java/com/example/ilian/Framework/Impl/TouchHandler.java
+++ app/src/main/java/com/example/ilian/Framework/Impl/TouchHandler.java
@@ -4,5 +4,19 @@ package com.example.ilian.Framework.Impl;
  * Created by ilian on 4/9/17.
  */
 
-public interface TouchHandler {
+import com.example.ilian.Framework.Input.TouchEvent;
+
+import java.util.List;
+import android.view.View.OnTouchListener;
+
+
+public interface TouchHandler extends OnTouchListener
+{
+    public boolean isTouchDown(int pointer);
+
+    public int getTouchX(int pointer);
+
+    public int getTouchY(int pointer);
+
+    public List<TouchEvent> getTouchEvents();
 }
