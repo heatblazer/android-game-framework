678fe33e0c536cbcb11ae9f941ca6fbfb2649b4a
ilian <heatblazer@gmail.com>
2017-04-11 19:24:04 +0300

    AndroidAudio: implemented

--- app/src/main/java/com/example/ilian/Framework/Impl/AndroidAudio.java
+++ app/src/main/java/com/example/ilian/Framework/Impl/AndroidAudio.java
@@ -4,22 +4,49 @@ package com.example.ilian.Framework.Impl;
  * Created by ilian on 4/7/17.
  */
 
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.*;
+import android.media.*;
+
+
 import com.example.ilian.Framework.Audio;
 import com.example.ilian.Framework.Music;
 import com.example.ilian.Framework.Voice;
 
 public class AndroidAudio implements Audio
 {
+    AssetManager assets;
+    SoundPool soundPool;
+
+    public AndroidAudio(Activity activity)
+    {
+        activity.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        assets = activity.getAssets();
+        soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC, 0);
+    }
 
     @Override
     public Music newMusic(String fname)
     {
-        return null;
+        try {
+            AssetFileDescriptor afd = assets.openFd(fname);
+            return new AndroidMusic(afd);
+        } catch (IOException ex) {
+            throw  new RuntimeException("Could not load music: " + fname);
+        }
     }
 
     @Override
     public Voice newSound(String fname)
     {
-        return null;
+        try {
+            AssetFileDescriptor afd = assets.openFd(fname);
+            int soundID = soundPool.load(afd, 0);
+            return  new AndroidVoice(soundPool, soundID);
+        } catch (IOException ex) {
+            throw new RuntimeException("Could not load voice: "+fname);
+        }
     }
 }
---
+++ app/src/main/java/com/example/ilian/Framework/Impl/AndroidFastRenderView.java
@@ -0,0 +1,8 @@
+package com.example.ilian.Framework.Impl;
+
+/**
+ * Created by ilian on 4/11/17.
+ */
+
+public class AndroidFastRenderView {
+}
