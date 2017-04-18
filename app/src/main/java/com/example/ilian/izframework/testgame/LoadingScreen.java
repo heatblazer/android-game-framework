cb5c4d117cb8aaf725f0fe7c52560036197bf7a9
ilian <heatblazer@gmail.com>
2017-04-18 18:07:51 +0300

    Test implementation: LoadingScreen

---
+++ app/src/main/java/com/example/ilian/izframework/testgame/LoadingScreen.java
@@ -0,0 +1,43 @@
+package com.example.ilian.izframework.testgame;
+
+/**
+ * Created by ilian on 4/18/17.
+ */
+
+import com.example.ilian.Framework.Graphics;
+import com.example.ilian.Framework.Graphics.PixmapFormat;
+import com.example.ilian.Framework.GameFramework;
+import com.example.ilian.Framework.Screen;
+
+
+public class LoadingScreen extends Screen
+{
+    public LoadingScreen(GameFramework game)
+    {
+        super(game);
+    }
+    @Override
+    public void update(float dTime) {
+
+    }
+
+    @Override
+    public void present(float dTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
